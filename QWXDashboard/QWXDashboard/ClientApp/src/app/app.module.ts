ebb58466d6298f7a50c144676a96ad2f403a599d
shaner92 <46929382+shaner92@users.noreply.github.com>
2019-03-12 22:32:43 -0400

    Implmented tree - need to populate with SQL data

--- QWXDashboard/QWXDashboard/ClientApp/src/app/app.module.ts
+++ QWXDashboard/QWXDashboard/ClientApp/src/app/app.module.ts
@@ -1,37 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from './material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { PartHistoryComponent } from './PartHistory/PartHistory.component';
+import { TreeDynamicExample } from './PartHistory/PartHistory.component';
 import { TrendBrowserComponent } from './trend-browser/trend-browser.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    PartHistoryComponent,
-    TrendBrowserComponent
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
-    MaterialModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'PartHistory', component: PartHistoryComponent },
-      { path: 'trend-browser', component: TrendBrowserComponent },
-    ])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent,
+        TreeDynamicExample,
+        TrendBrowserComponent
+    ],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+        BrowserAnimationsModule,
+        HttpClientModule,
+        FormsModule,
+        MaterialModule,
+        MatNativeDateModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot([
+            { path: '', component: HomeComponent, pathMatch: 'full' },
+            { path: 'PartHistory', component: TreeDynamicExample },
+            { path: 'trend-browser', component: TrendBrowserComponent },
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
