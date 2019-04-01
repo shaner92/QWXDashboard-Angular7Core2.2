f131b274ddf50933d085d8865c7a173efe34a1f4
shaner92 <46929382+shaner92@users.noreply.github.com>
2019-04-01 16:06:44 -0400

    Started firebase authentication

--- QWXDashboard/QWXDashboard/ClientApp/src/app/Login/Login.component.ts
+++ QWXDashboard/QWXDashboard/ClientApp/src/app/Login/Login.component.ts
@@ -1,16 +1,19 @@
 import { Component, Injectable, Inject, OnInit, AfterContentInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { AuthService } from '../Services/AuthService.service';
 
 @Component({
     selector: 'app-Login',
     templateUrl: './Login.component.html',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
     
-    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    constructor(private authService: AuthService) {
 
     }
+
+    ngOnInit() {}
 
    
 
--- QWXDashboard/QWXDashboard/ClientApp/src/app/app.module.ts
+++ QWXDashboard/QWXDashboard/ClientApp/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule, AngularFireAuth } from "@angular/fire/auth";
 
 import { MaterialModule } from './material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,10 +11,8 @@ import { MatNativeDateModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
-import { UserComponent } from './User/User.component';
-import { RegistrationComponent } from './User/Registration/Registration.component';
-import { LoginComponent } from './User/Login/Login.component';
+import { RegistrationComponent } from './Registration/Registration.component';
+import { LoginComponent } from './Login/Login.component';
 
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -21,11 +21,19 @@ import { TrendBrowserComponent } from './trend-browser/trend-browser.component';
 import { FooterComponent } from './footer/footer.component';
 import { AdminToolsComponent } from './AdminTools/AdminTools.component';
 
+var config = {
+    apiKey: "AIzaSyAW_dYLyqzo45nsqTZkvq29JA6Mh_2GHIE",
+    authDomain: "qualitydashboard-65246.firebaseapp.com",
+    databaseURL: "https://qualitydashboard-65246.firebaseio.com",
+    projectId: "qualitydashboard-65246",
+    storageBucket: "qualitydashboard-65246.appspot.com",
+    messagingSenderId: "162522009619"
+};
+
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        UserComponent,
         RegistrationComponent,
         LoginComponent,
         HomeComponent,
@@ -36,6 +44,8 @@ import { AdminToolsComponent } from './AdminTools/AdminTools.component';
     ],
     imports: [
         BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+        AngularFireModule.initializeApp(config),
+        AngularFireAuthModule,
         HttpClientModule,
         BrowserAnimationsModule,
         FormsModule,
@@ -43,15 +53,8 @@ import { AdminToolsComponent } from './AdminTools/AdminTools.component';
         MatNativeDateModule,
         ReactiveFormsModule,
         RouterModule.forRoot([
-            { path: '', redirectTo: '/User/Registration', pathMatch: 'full' },
-            {
-                path: 'User', component: UserComponent,
-                children: [
-                    { path: 'Registration', component: RegistrationComponent },
-                    { path: 'Login', component: LoginComponent }
-                ]
-            },
-               
+            { path: '', component: LoginComponent , pathMatch: 'full' },
+            { path: 'Registration', component: RegistrationComponent },
             { path: 'Home', component: HomeComponent},
             { path: 'PartHistory', component: PartHistoryComponent },
             { path: 'trend-browser', component: TrendBrowserComponent },
