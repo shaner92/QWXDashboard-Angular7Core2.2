692644ec859de4684c5a26a38f5934c9529897f4
shaner92 <46929382+shaner92@users.noreply.github.com>
2019-04-11 10:27:12 -0400

    Fix

--- QWXDashboard/QWXDashboard/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ QWXDashboard/QWXDashboard/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -8,8 +8,8 @@ import { AuthService } from '../Services/AuthService.service';
 })
 export class NavMenuComponent implements OnInit{
   isExpanded = false;
-  authService: AuthService;
-    constructor() {
+ 
+    constructor(private authService : AuthService) {
 
     }
 
